9aedc582d44c36ce028639335f4aa6b33c2f34f8
https://github.com/GoncalvesFilipe <filipe.tec.informatica.if@gmail.com>
2025-10-17 12:03:37 -0300

    index updates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# PÁGINA INICIAL DO SITE
+def index(request):
+    return render(request, "app_inventario/index.html")  # Renderiza o template da página inicial
