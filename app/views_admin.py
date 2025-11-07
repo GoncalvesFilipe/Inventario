07943b200c450d9efcbaa6ba9c4521bc2be7665e
https://github.com/GoncalvesFilipe <filipe.tec.informatica.if@gmail.com>
2025-11-07 10:10:38 -0300

    Inserting delta function into assets and route correction

--- app/views_admin.py
+++ app/views_admin.py
@@ -3,6 +3,7 @@ from .forms import PatrimonioForm
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from .models import Usuario, Patrimonio
+from django.views.decorators.csrf import csrf_exempt
 
 # Página principal do painel
 def admin_dashboard(request):
@@ -18,42 +19,24 @@ def patrimonios_list(request):
     patrimonios = Patrimonio.objects.select_related("usuario").all()
     return render(request, "app_inventario/partials/patrimonio_list.html", {"patrimonios": patrimonios})
 
-# Exibir formulário de novo patrimônio
 def patrimonio_form(request):
-    if request.method == "POST":
-        form = PatrimonioForm(request.POST)
-        if form.is_valid():
-            form.save()
-            patrimonios = Patrimonio.objects.all()
-            return render(request, "app_inventario/partials/tabela_patrimonios.html", {"patrimonios": patrimonios})
-    else:
-        form = PatrimonioForm()
-    
+    """Exibe o formulário vazio para adicionar um novo patrimônio."""
+    form = PatrimonioForm()
     return render(request, "app_inventario/partials/form_patrimonio.html", {"form": form})
 
-
-# Adicionar patrimônio
+# Adicionar novo patrimônio
 def patrimonio_add(request):
     if request.method == "POST":
         form = PatrimonioForm(request.POST)
         if form.is_valid():
             form.save()
-
             patrimonios = Patrimonio.objects.select_related("usuario").all()
-            tabela_html = render_to_string(
-                "app_inventario/partials/tabela_patrimonios.html",
-                {"patrimonios": patrimonios},
-                request=request
-            )
+            return render(request, "app_inventario/partials/tabela_patrimonios.html", {"patrimonios": patrimonios})
+    else:
+        form = PatrimonioForm()
+    return render(request, "app_inventario/partials/form_patrimonio.html", {"form": form})
 
-            html_final = (
-                tabela_html
-                + '<div id="form-patrimonio-container" hx-swap-oob="true"></div>'
-            )
 
-            return render(request, "app_inventario/partials/form_patrimonio.html", {"form": form})
-
-        
 # Editar patrimônio
 def patrimonio_edit(request, pk):
     patrimonio = get_object_or_404(Patrimonio, pk=pk)
@@ -61,16 +44,31 @@ def patrimonio_edit(request, pk):
         form = PatrimonioForm(request.POST, instance=patrimonio)
         if form.is_valid():
             form.save()
-            patrimonios = Patrimonio.objects.all()
+            patrimonios = Patrimonio.objects.select_related("usuario").all()
             return render(request, "app_inventario/partials/tabela_patrimonios.html", {"patrimonios": patrimonios})
     else:
         form = PatrimonioForm(instance=patrimonio)
+    return render(request, "app_inventario/partials/form_patrimonio.html", {"form": form, "patrimonio": patrimonio})
 
-    return render(
-        request,
-        "app_inventario/partials/form_patrimonio.html",
-        {"form": form, "patrimonio": patrimonio}
-    )
+    
+# Confirma exclusão de patrimônio
+def confirmar_exclusao_patrimonio(request, pk):
+    patrimonio = get_object_or_404(Patrimonio, pk=pk)
+    return render(request, "app_inventario/partials/confirmar_exclusao_patrimonio.html", {"patrimonio": patrimonio})
+
+
+# Excluir patrimônio
+@csrf_exempt
+def excluir_patrimonio(request, pk):
+    """Exclui um patrimônio via HTMX (POST ou DELETE)."""
+    if request.method in ["POST", "DELETE"]:
+        patrimonio = get_object_or_404(Patrimonio, pk=pk)
+        patrimonio.delete()
+
+        patrimonios = Patrimonio.objects.select_related("usuario").all()
+        return render(request, "app_inventario/partials/tabela_patrimonios.html", {"patrimonios": patrimonios})
+
+    return HttpResponse(status=405)
         
 # Edição de usuário
 def usuario_edit(request, pk):
@@ -88,10 +86,10 @@ def usuario_edit(request, pk):
     return render(request, "app_inventario/partials/form_usuario.html", {"usuario": usuario})
 
 # Exclusão de usuário
+@csrf_exempt
 def usuario_delete(request, pk):
     usuario = get_object_or_404(Usuario, pk=pk)
-
-    if request.method == 'POST':
+    if request.method in ['POST', 'DELETE']:
         usuario.delete()
         usuarios = Usuario.objects.all().order_by("nome")
         return render(request, "app_inventario/partials/usuarios_list.html", {"usuarios": usuarios})
